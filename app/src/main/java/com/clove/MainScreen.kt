507e10f78b5a9a0b011b80fe7804514fa255e26b
Arman Singh <armansinghof@gmail.com>
2025-06-05 22:51:10 +0530

    Modified into Floating Navigation Bar.

--- app/src/main/java/com/clove/MainScreen.kt
+++ app/src/main/java/com/clove/MainScreen.kt
@@ -1,15 +1,44 @@
 package com.clove
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.clove.navigation.BottomNavBar
 import com.clove.navigation.NavigationHost
+
+//@Composable
+//fun MainScreen() {
+//    val navController = rememberNavController()
+//    val backStackEntry = navController.currentBackStackEntryAsState()
+//    val currentRoute = backStackEntry.value?.destination?.route
+//
+//    Scaffold(
+//        contentWindowInsets = androidx.compose.foundation.layout.WindowInsets(0),
+//        bottomBar = {
+//            BottomNavBar(
+//                navController = navController,
+//                currentRoute = currentRoute
+//            )
+//        }
+//    ) { innerPadding ->
+//        Box(
+//            modifier = Modifier
+//                .fillMaxSize()
+//                .padding(innerPadding)
+//        ) {
+//            NavigationHost(navController)
+//        }
+//    }
+//}
 
 @Composable
 fun MainScreen() {
@@ -18,13 +47,8 @@ fun MainScreen() {
     val currentRoute = backStackEntry.value?.destination?.route
 
     Scaffold(
-        contentWindowInsets = androidx.compose.foundation.layout.WindowInsets(0),
-        bottomBar = {
-            BottomNavBar(
-                navController = navController,
-                currentRoute = currentRoute
-            )
-        }
+        contentWindowInsets = WindowInsets(0),
+        containerColor = MaterialTheme.colorScheme.background
     ) { innerPadding ->
         Box(
             modifier = Modifier
@@ -32,6 +56,19 @@ fun MainScreen() {
                 .padding(innerPadding)
         ) {
             NavigationHost(navController)
+
+            // Floating Bottom Navigation Bar
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(bottom = 70.dp),
+                contentAlignment = Alignment.BottomCenter
+            ) {
+                BottomNavBar(
+                    navController = navController,
+                    currentRoute = currentRoute
+                )
+            }
         }
     }
 }
